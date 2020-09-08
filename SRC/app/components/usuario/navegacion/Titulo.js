6d5f3b5e74f99ea77dffb9f3dd3b5bdadc6c589c
vicente <viz.rh2@gmail.com>
2020-09-07 20:48:39 -0500

    se prueban complementos sobre VSc generando nuevos archivos

--- SRC/app/components/usuario/navegacion/Titulo.js
+++ SRC/app/components/usuario/navegacion/Titulo.js
@@ -5,8 +5,8 @@ function Titulo(props) {
   
     return (
         <div className="app-title-viz">
-            <div onClick="regresar();" className="regresar app-title-button-back"> 
-              <span className="fa  fa-angle-left"></span>
+            <div  className="regresar app-title-button-back rounded-circle"> 
+              <span className="fa  fa-angle-left "></span>
             </div>
             <h3 className="title-text" >RestaurApp</h3>
 	    	</div>
